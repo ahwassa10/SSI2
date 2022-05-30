c6fdbc6d2ec02f812b6a80b300368e1268add13e
Paul Kotys <71190521+ahwassa10@users.noreply.github.com>
2022-05-29 21:16:46 -0400

    Created custom item. Metal Detector

--- src/main/java/com/shrapnitek/ssi2/items/ModItems.java
+++ src/main/java/com/shrapnitek/ssi2/items/ModItems.java
@@ -1,15 +1,20 @@
 package com.shrapnitek.ssi2.items;
 
 import com.shrapnitek.ssi2.SSI2;
+import com.shrapnitek.ssi2.items.custom.MetalDetector;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item PURPLE_GEM = registerItem("purple_gem",
             new Item(new FabricItemSettings().group(ModItemGroup.SSI2_ITEMGROUP)));
+
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetector(new FabricItemSettings()
+                    .group(ModItemGroup.SSI2_ITEMGROUP)
+                    .maxDamage(32)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(SSI2.MOD_ID, name), item);
